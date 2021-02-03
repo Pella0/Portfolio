c1bf3aa0707805de663da6e25348102ea147e89c
Pella0 <pellattiero.ju@gmail.com>
2021-02-03 12:18:32 +0100

    creation route app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import './App.css';
+/* import Navbar from './Navbar'; */
+import { Switch, Route } from 'react-router-dom';
+import { Router } from 'react-router';
 
-function App() {
-  return <div className="App">Wild P3</div>;
-}
+const App = () => {
+  return (
+    <div className="global-page">
+      <Router>
+        {/* <Navbar /> */}
+        <div className="App">
+          <Switch>
+            <Route exact path="/" />
+            <Route exact path="/customer/:id" /* component={Customer} */ />
+            <Route exact path="/work/:id" /* component={Work} */ />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
